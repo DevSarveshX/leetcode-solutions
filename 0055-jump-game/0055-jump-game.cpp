d88996423b2ffa264fc6147004ac99b6ada2634d
DevSarveshX <sarvesh0224@gmail.com>
2025-12-13 23:30:16 +0530

    Time: 0 ms (100%), Space: 52.3 MB (65.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-         int max_index_covered = 0; 
-        for (int i = 0; i < nums.size(); i++) {
+          int max_index_covered = 0; 
+         for (int i = 0; i < nums.size(); i++) {
             if (i > max_index_covered){
-             return false;
-            }
-            max_index_covered = max(max_index_covered, i + nums[i]); 
+              return false;
+             }
+             max_index_covered = max(max_index_covered, i + nums[i]); 
         }
         return true; 
+
+
+       
     }
 };
