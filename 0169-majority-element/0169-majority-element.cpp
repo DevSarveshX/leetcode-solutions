c47b4fda87962ccf9fde475258e3546c0a89c92e
DevSarveshX <sarvesh0224@gmail.com>
2026-07-31 14:48:19 +0530

    Time: 4 ms (18.85%), Space: 28.3 MB (39.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -6,11 +6,11 @@ public:
         unordered_map<int,int>freq;
         for(int i=0; i<n; i++){
               freq[nums[i]]++;
-            if(freq.find(nums[i])!=freq.end()){
+           
                 if(freq[nums[i]]>n/2){
                     ans= nums[i];
                     break;
-                }
+            
             }
         }
         return ans;
