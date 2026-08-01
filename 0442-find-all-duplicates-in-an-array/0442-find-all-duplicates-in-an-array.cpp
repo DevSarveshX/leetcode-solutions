b27a0d3b0cc3184269c3d87aa04e834795eed1b0
DevSarveshX <sarvesh0224@gmail.com>
2026-08-01 11:16:31 +0530

    Time: 58 ms (21.26%), Space: 67.2 MB (8.04%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-       
-        // vector<int>ans;
-        // for(int i=1; i<nums.size(); i++){
-        //     if(nums[i]==nums[i-1]){
-        //         ans.push_back(nums[i]);
-                
-        //     }
-        // }
-        //     return ans;
-   // Method 2
-        //first find the max element in nums
+        unordered_map<int, int>mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
         vector<int>ans;
-        int max= INT_MIN;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]>max){
-                max=nums[i];
+        for(auto it:mp){
+                if(it.second>1){
+                    ans.push_back(it.first);
+                }
             }
-        }
-        int n=max;
-        vector<int>freq(n+1,0);
-        for(int i=0; i<nums.size(); i++){
-            freq[nums[i]]++;
-        }
-        for(int i=0; i<freq.size();i++){
-            if(freq[i]>1){
-                ans.push_back(i);
-            }
-        }
+        
         return ans;
     }
-   
 };
