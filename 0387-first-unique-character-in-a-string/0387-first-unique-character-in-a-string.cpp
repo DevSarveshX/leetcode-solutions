3c4c863c5d904c9147288741ea8271ba2de8f339
DevSarveshX <sarvesh0224@gmail.com>
2026-08-01 14:45:34 +0530

    Time: 9 ms (53.9%), Space: 14.8 MB (56.46%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -8,9 +8,9 @@ public:
         for(int i=0; i<s.size(); i++){
             if(mp[s[i]]==1){
                 return i;
-                break;
             }
         }
         return -1;
     }
 };
+
